c4188902a1d2ddfbe49598fb06283b671964f592
Walid Djemmaa <walid@jarvis-legal.com>
2024-12-14 04:25:56 +0100

    changed language

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     app: {
         token: process.env.DISCORD_TOKEN || 'xxx',
-        playing: 'by the Community ❤️',
+        playing: '/play for music',
         global: true,
         guild: process.env.GUILD_ID || 'xxx',
         extraMessages: false,
         loopMessage: false,
-        lang: 'en',
+        lang: 'fr',
         enableEmojis: false,
     },
 
